6b5c2bb7391b062c8b071e0f31bffcb224b0e7c2
RAFAELA <72530170+RafaelaCugini@users.noreply.github.com>
2021-06-14 22:02:17 -0300

    first commit

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,31 @@
 const cipher = {
-  // ...
-};
+  encode: function (offset, string) {
+    let code = "";
+
+    for (let i = 0; i < string.length; i++) {        // i vai ser o valor da letra
+     let codeMensagem = string.charCodeAt(i);        // busca no site 
+      if (codeMensagem >= 65 && codeMensagem <= 90) {
+       let encript = ((codeMensagem - 65 + offset) %26 ) + 65;
+       code += String.fromCharCode(encript);
+    }
+  }
+  console.log (code);
+  return code;
+  },
+
+  decode: function (offset, string) {
+  let decode = "";
+
+  for (let i = 0; i < string.length; i++) {        // i vai ser o valor da letra
+   let decodeMensagem = string.charCodeAt(i);        // busca no site 
+    if (decodeMensagem <= 65 && decodeMensagem <= 90) {
+     let decript = ((decodeMensagem - 65 + offset) %26 ) + 65;
+     decode += String.fromCharCode(decript);
+  }
+  }
+  console.log (decode);
+  return decode;
+  }
+}
 
 export default cipher;
