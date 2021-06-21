1eb8b300f778adeaf693ed3b8e229ec8341b6f7b
RAFAELA <72530170+RafaelaCugini@users.noreply.github.com>
2021-06-21 14:59:09 -0300

    second commit

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,8 @@
 const cipher = {
   encode: function (offset, string) {
+    if (typeof offset !== 'number' || typeof string !== 'string') {
+      throw new TypeError
+    }
     let code = "";
 
     for (let i = 0; i < string.length; i++) {        // i vai ser o valor da letra
@@ -9,23 +12,26 @@ const cipher = {
        code += String.fromCharCode(encript);
     }
   }
-  console.log (code);
+  //console.log (code);
   return code;
   },
 
   decode: function (offset, string) {
+    if (typeof offset !== 'number' || typeof string !== 'string') {
+      throw new TypeError
+    }
   let decode = "";
 
   for (let i = 0; i < string.length; i++) {        // i vai ser o valor da letra
    let decodeMensagem = string.charCodeAt(i);        // busca no site 
-    if (decodeMensagem <= 65 && decodeMensagem <= 90) {
-     let decript = ((decodeMensagem - 65 + offset) %26 ) + 65;
+    if (decodeMensagem >= 65 && decodeMensagem <= 90) {
+     let decript = ((decodeMensagem - 90 - offset) %26 ) + 90;
      decode += String.fromCharCode(decript);
   }
   }
-  console.log (decode);
+  //console.log (decode);
   return decode;
   }
-}
+};
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -3,85 +3,21 @@ import cipher from './cipher.js';
 const botaocod = document.getElementById("cripto");
 botaocod.addEventListener("click",event => {
     event.preventDefault()                                     // previne o carregamento da pagina
-    let offset = document.getElementById("offset").value;         // deslocamento 
+    let offset = Number(document.getElementById("offset").value);         // deslocamento 
     let text = document.getElementById("text").value;           // pegar a mensagem
     let numOffset = parseInt(offset);                            //mensagem para numero
     document.getElementById("code").innerHTML=cipher.encode(numOffset, text);
-    console.log (botaocod);
+    //console.log (botaocod);
 });
 
 const botaodec = document.getElementById("decripto");
 botaodec.addEventListener("click",event => {
     event.preventDefault()                                     // previne o carregamento da pagina
-    let offset = document.getElementById("offset").value;         // deslocamento 
+    let offset = Number(document.getElementById("offset").value);         // deslocamento 
     let text = document.getElementById("text").value;           // pegar a mensagem
     let numOffset = parseInt(offset);                            //mensagem para numero
     document.getElementById("code").innerHTML=cipher.decode(numOffset, text);
-    console.log (botaodec);
+    //console.log (botaodec);
 });
 
-console.log(cipher);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* let cifra = prompt("Gostaria de inicar a Criptografia? Responda com sim ou não");
-console.log(cifra); 
-
-if (cifra.toLowerCase() == "não") {
-        alert("Então tchau!");
-}
-else if (cifra.toLowerCase() === "sim") {
-        alert("Coloque a mensagem que gostaria de enviar")
-};
-
-function cipher(str, num) {
-    num = num % 26;
-    var lowerCaseStr = str.tolowerCase();
-    var alphabet = 'abcdefghijklmnopqrstuvwxyz'.s[lit('')];
-    var newStr = '';
-
-    for(var i = 0; i < lowerCaseStr.length; i++) {
-        var currentLetter = lowerCaseStr[i];
-        if(currentLetter === ' ') {
-            newStr += currentLetter;
-            continue;
-        }
-        var currentIndex = alphabet.indexOf(currentLetter);
-        var newIndex = currentIndex + num;
-        if(newIndex > 25) newIndex = newIndex - 26;
-        if(newIndex < 0) newIndex = newIndex + 26;
-        if(str[i] === str[i].toUpperCase()) {
-            newStr += alphabet[newIndex].toUpperCase();
-        }
-        else newStr += alphabet[newIndex];
-    }
-    return newStr;
-}
-
-/* let (CodigoDaletra + 7 ) % 26
-((codigoDaletraASC - cod1aLetra + desloc) % tamDoalfabeto) + cod1aLetra;
-
-function cifra(string) {
-    return
-}*/
 //console.log(cipher);
